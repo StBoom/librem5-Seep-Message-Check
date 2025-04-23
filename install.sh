a85c33faa3edee83110337c12bb2dadb49625261
StBoom <72914136+StBoom@users.noreply.github.com>
2025-04-23 12:07:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Installer for Wakeup Check Service
 
 # Define paths
-SCRIPT_PATH="/usr/local/bin/wakeup-check.sh"
+SCRIPT_PATH="/usr/local/bin/"
 LOG_FILE="/var/log/wakeup-check.log"
 WAKE_TIMESTAMP_FILE="/var/lib/wakeup-check/last_wake_timestamp"
 SERVICE_PRE_PATH="/etc/systemd/system/wakeup-check-pre.service"
