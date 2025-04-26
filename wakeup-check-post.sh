a97867a18c72b16c930f114e9b1a5ee371802b87
StBoom <72914136+StBoom@users.noreply.github.com>
2025-04-26 23:16:28 +0200

    Update wakeup-check-post.sh

--- wakeup-check-post.sh
+++ wakeup-check-post.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-
 case $1/$2 in
   pre/*)
-    # Vor dem Schlafen (optional)
+    # Vor dem Schlafen: wakeup-check-pre.service starten
+    systemctl start wakeup-check-pre.service
     ;;
   post/*)
-    # Nach dem Aufwachen
-    /usr/local/bin/wakeup-check.sh post
+    # ... (restlicher Code wie oben)
     ;;
+esac
